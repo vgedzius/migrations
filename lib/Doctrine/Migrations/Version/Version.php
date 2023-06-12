6c4387ce45c76d0076e1732948132a2cb455d98b
Vaidotas <vaidotas@gedzius.lt>
2023-06-12 13:49:19 +0700

    Attribute added to the wrong class

--- lib/Doctrine/Migrations/Version/Version.php
+++ lib/Doctrine/Migrations/Version/Version.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Doctrine\Migrations\Version;
 
-use Symfony\Component\Console\Attribute\AsCommand;
-
-#[AsCommand(name: 'migrations:version', description: 'Manually add and delete migration versions from the version table.')]
 final class Version
 {
     private string $version;
